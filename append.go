f5a835a856be69f8845ac2babd9006760b0e9eda
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-04-27 17:28:23 +0900

    Fix append operation

--- append.go
+++ append.go
@@ -6,6 +6,13 @@ import (
 )
 
 func Append(path string, val string) error {
+	// Make diary file
+	if !isFileExist(path) {
+		if err := makeFile(path); err != nil {
+			return fmt.Errorf("Failed make diary file. %s", err.Error())
+		}
+	}
+
 	// Append content
 	file, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
